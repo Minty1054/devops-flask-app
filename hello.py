0508321790c0e5cf33f4559f60a3b99b716d96aa
Mahi Rahman <c24472796@myTUDublin.ie>
2026-09-25 10:28:09 +0100

    Add /contact page showing my email, linked from the homepage

--- hello.py
+++ hello.py
@@ -5,11 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-    return '<p>Hello, World, I am a Flask app!</p><p><a href="/about">About this app</a></p>'
+    return ('<p>Hello, World, I am a Flask app!</p>'
+            '<p><a href="/about">About this app</a></p>'
+            '<p><a href="/contact">Contact</a></p>')
 
 
 @app.route('/about')
 def about():
     return ('<p>This application is running on the Flask web framework.</p>'
             '<p>Learn more about Flask: <a href="https://flask.palletsprojects.com/">https://flask.palletsprojects.com/</a></p>'
+            '<p><a href="/">Back to home</a></p>')
+
+
+@app.route('/contact')
+def contact():
+    return ('<p>Contact me at: <a href="mailto:r19mahi@gmail.com">r19mahi@gmail.com</a></p>'
             '<p><a href="/">Back to home</a></p>')
